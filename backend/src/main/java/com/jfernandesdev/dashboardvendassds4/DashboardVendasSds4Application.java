02996826f5b68c7990a4a46fe4519f8e55e6f25f
jfernandesdev <jfernandes.dev@gmail.com>
2021-09-06 16:03:01 -0300

    project creation

---
+++ backend/src/main/java/com/jfernandesdev/dashboardvendassds4/DashboardVendasSds4Application.java
@@ -0,0 +1,13 @@
+package com.jfernandesdev.dashboardvendassds4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardVendasSds4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardVendasSds4Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jfernandesdev/dashboardvendassds4/DashboardVendasSds4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfernandesdev.dashboardvendassds4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardVendasSds4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
