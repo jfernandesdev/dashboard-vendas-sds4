9ab0b76ffadcd7a887bc472eaf18b31c6dd9a58c
jfernandesdev <jfernandes.dev@gmail.com>
2021-09-10 15:49:59 -0300

    home, navigation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
-    <h1>Página Home</h1>
+    <>
+      <Navbar />
+      <div className="container p-5">
+        <div className="jumbotron">
+          <h1 className="display-4">Dashboard de Vendas - SDS#4</h1>
+          <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+          <hr />
+          <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back-end construído com Spring Boot.</p>
+          <Link className="btn btn-danger btn-lg my-3" to='/dashboard'>Ir para o Dashboard</Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
